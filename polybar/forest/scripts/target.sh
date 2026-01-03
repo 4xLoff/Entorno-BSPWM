f66c07eeb6eaf8c058c83ce19142fd3842c35150
4xL <77083140+4xLoff@users.noreply.github.com>
2026-01-03 16:10:04 -0500

    Delete polybar directory

--- polybar/forest/scripts/target.sh
+++
@@ -1,8 +0,0 @@
-target=$(cat /tmp/name)
-
-if [ "$target" = "Null" ]; then
-  echo "Waiting..."
-else  
-  echo "%{F#9658A4}   %{F#93A1A1}$(cat /tmp/target)  %{F#9658A4}  %{F#93A1A1}$(cat /tmp/name)"
-fi
-
--- polybar/scripts/ethernet_status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig ens33 | grep "inet " | awk '{print $2}')%{u-}"
-
-
-IP=$(ip route get 1.1.1.1 | awk '{print $7}')
-echo "%{F#2495e7} %{F#ffffff}$IP%{u-}"
-
