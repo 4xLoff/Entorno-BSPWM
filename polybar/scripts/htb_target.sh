8a58ff8a633691f1cc42846cbe33a578fa390daa
4xL <77083140+4xLoff@users.noreply.github.com>
2026-01-04 23:10:55 -0500

    rework htb_target

--- polybar/scripts/htb_target.sh
+++ polybar/scripts/htb_target.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-target="$(cat /tmp/target 2>/dev/null | xargs)"  # xargs elimina espacios y saltos
 name="$(cat /tmp/name 2>/dev/null | xargs)"    # xargs elimina espacios y saltos
+target="$(cat /tmp/target 2>/dev/null | xargs)"  # xargs elimina espacios y saltos
 
 # Si los archivos contienen "Null" (texto) o están vacíos, mostrar "Waiting..."
 if [[ "$target" == "Null" || "$name" == "Null" || -z "$target" || -z "$name" ]]; then
   echo "%{F#e51d0b}ﲅ %{u-}%{F#ffffff} Waiting..."
 elif [[ -n "$target" && -n "$name" ]]; then
-  echo "%{F#e51d0b} %{F#ffffff}${target}  %{F#e51d0b}  %{F#ffffff}${name}"
+  echo "%{F#e51d0b} %{F#ffffff}${name}  %{F#e51d0b}  %{F#ffffff}${target}"
 else
   echo "%{F#e51d0b}ﲅ %{u-}%{F#ffffff} Waiting..."
 fi
