bfa12d9e958d171ed356299ae66df5b923b2e647
4xL <77083140+4xLoff@users.noreply.github.com>
2026-01-14 00:53:37 -0500

    Delete polybar/emili directory

--- polybar/emili/scripts/results.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-readonly PATH_ARCHIVE="${HOME}/.config/bin/updates.txt"
-
-readonly packages="$(grep -oP '\d+(?= paquetes| packages)' "${PATH_ARCHIVE}" 2>/dev/null)"
-
-echo "${packages:-0}"
